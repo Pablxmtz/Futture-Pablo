3de9c133722ce02752ebd27e95efa4bd58e3ad81
Pablxmtz <ax10Pablo@hotmail.es>
2020-05-12 18:32:22 +0200

    Home sin galería

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/Tipos');
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   return {
