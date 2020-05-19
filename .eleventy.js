28320dfeffab4c535050b8d82c003656aab98c8b
Pablxmtz <ax10Pablo@hotmail.es>
2020-05-19 17:17:52 +0200

    todo menos cesta y responsive

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/Tipos');
+  eleventyConfig.addPassthroughCopy('src/scripts/slick');
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   return {
